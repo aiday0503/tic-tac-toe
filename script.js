3919355488efc7001bed665252b386b37f2ee863
maratgaip <marattig@gmail.com>
2017-07-26 21:38:10 -0700

    feat(eventListener added)

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-/**
- * Created by marat.gaipov on 7/26/17.
- */
+$(document).ready(function() {
+
+	// to switch players.
+	let player1 = true;
+
+	$('.box').on('click', function() {
+		// if box is not clicked yet
+		if (!$(this).hasClass('clicked')) {
+			// add extra class and switch player.
+			$(this).addClass('box clicked');
+			player1 = !player1;
+		}
+	});
+
+});
